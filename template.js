8fcfa6bea69a75169e1ff70eab94b03de3d77612
habibesayrac@protekenerji.com <habibesayrac@outlook.com>
2023-07-02 14:50:32 +0300

    Search,DarkMode,Header,Footer

--- template.js
+++ template.js
@@ -1,3 +1,29 @@
 //alert("Deneme")
-//window.alert("Deneme")
-//window.alert("Deneme")
+//Dark Mode
+let darkModu = () => {
+    //alert("Dark modu");
+    window.document.classList.toggle("dark_mode")
+}
+//input search
+$(document).ready(function () {
+    const searchApi = ["Adana", "Balikesir", "Çorum", "Denizli", "Diyarbakir", "Edirne", "Malatya", "Van"];
+    $("#tags").autocomplete({
+        source: searchApi
+    })
+
+});
+//end 
+
+//Footer
+let newDate = () => {
+    const date = new Date().getFullYear();
+    //DOM
+    // document.getElementById("date_id").innerHTML=new Date().getFullYear();
+    //Jquery DOM
+    $("#date_id").html(date);
+
+}
+newDate()
+
+
+    //JQueryde dom nasıl çalışır araştır
