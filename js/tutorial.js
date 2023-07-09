c6b7647debf842179071b7ab113a5ff4c7ac3680
habibesayrac@protekenerji.com <habibesayrac@outlook.com>
2023-07-09 13:38:41 +0300

    whoisting, variables

--- js/tutorial.js
+++ js/tutorial.js
@@ -2,13 +2,44 @@
 /*
 multiple comment
 */ 
-console.log("loglama");
-console.log("info");
-console.warn("warn");
-console.error("error");
+// console.log("loglama");
+// console.log("info");
+// console.warn("warn");
+// console.error("error");
 
-alert("pop-up bilgilendirme-1");
-window.alert("pop-up bilgilendirme-2");
+// alert("pop-up bilgilendirme-1");
+// window.alert("pop-up bilgilendirme-2");
 
-window.document.writeln("ekranda yazı");
+// window.document.writeln("ekranda yazı");
 
+// variable
+var result = "Js öğreniyorum";
+console.log(typeof result+"=>"+result);
+
+var result = 4423;
+console.log(typeof result+"=>"+result);
+
+var result = 44.23;
+console.log(typeof result+"=>"+result);
+
+var result = true;
+console.log(typeof result+"=>"+result);
+
+//Naming Convensition
+var deneme = "JS Data";
+console.log(deneme);
+
+var deneme44 = "JS Data";
+console.log(deneme44);
+
+var $deneme44 = "JS Data";
+console.log(deneme44);
+
+var denemeVerisi = "JS Data";
+console.log(denemeVerisi);
+
+//whoisting
+result2=56;
+var result2;
+
+//Operators
