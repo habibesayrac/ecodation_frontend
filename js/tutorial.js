8856eaae0b512bb167b15cc8dde0e1a0f9dd50ca
habibesayrac@protekenerji.com <habibesayrac@outlook.com>
2023-07-10 00:09:01 +0300

    operators

--- js/tutorial.js
+++ js/tutorial.js
@@ -13,33 +13,108 @@ multiple comment
 // window.document.writeln("ekranda yazı");
 
 // variable
-var result = "Js öğreniyorum";
-console.log(typeof result+"=>"+result);
+// var result = "Js öğreniyorum";
+// console.log(typeof result+"=>"+result);
 
-var result = 4423;
-console.log(typeof result+"=>"+result);
+// var result = 4423;
+// console.log(typeof result+"=>"+result);
 
-var result = 44.23;
-console.log(typeof result+"=>"+result);
+// var result = 44.23;
+// console.log(typeof result+"=>"+result);
 
-var result = true;
-console.log(typeof result+"=>"+result);
+// var result = true;
+// console.log(typeof result+"=>"+result);
 
-//Naming Convensition
-var deneme = "JS Data";
-console.log(deneme);
+// //Naming Convensition
+// var deneme = "JS Data";
+// console.log(deneme);
 
-var deneme44 = "JS Data";
-console.log(deneme44);
+// var deneme44 = "JS Data";
+// console.log(deneme44);
 
-var $deneme44 = "JS Data";
-console.log(deneme44);
+// var $deneme44 = "JS Data";
+// console.log(deneme44);
 
-var denemeVerisi = "JS Data";
-console.log(denemeVerisi);
+// var denemeVerisi = "JS Data";
+// console.log(denemeVerisi);
 
-//whoisting
-result2=56;
-var result2;
+// //whoisting
+// result2=56;
+// var result2;
 
 //Operators
+//+ - * ** / % 
+//! not &&and || or
+//x++
+//x--
+//++x
+//--x
+//= (atama)
+//== (türüne bakma)
+//===(türüne bak)
+//
+// var userData =Number(prompt("Lütfen sayı giriniz"));
+// console.log(userData+45);
+
+
+// //undefined
+// //NaN
+// //isNaN
+// //Infinity
+
+// //Math
+// console.log(Math.PI);
+
+// var sayi =-5.9;
+// console.log(Math.abs(sayi));
+
+//condition
+// const condi=()=>{
+//     let number =4;
+//     if(number>=2){
+//         console.log("Büyüktür");
+//     }else{
+//        console.log("Küçüktür");
+//     }
+// }
+// condi();
+     
+// let tryCatchTuto=()=>{
+//     try {
+//         alertx("pop-up");
+//     } catch (error) {
+//         console.log("hata mesajı: "+ error.message);
+//         console.log("hata mesajı: "+ error.name);
+//         console.log(error);
+//     }
+   
+// console.log("son satır");
+
+// }
+// tryCatchTuto();
+
+// let sumExamplesData=()=>{
+    
+//     let commonSum=0;
+//     let oddSum=0, oddCounter=0, oddNumber=0;
+//     let evenSum=0, evenCounter=0, evenNumber=0;
+
+//     //userData
+//     let user =Number(prompt("Lütfen Bitiş sayısı yazınız"));
+
+//     for (let i = 1; i <= user; i++){
+//         if (user===44){
+//             console.log("secret key number failed" +user);
+//         break;
+//             }
+//     if (i===50){
+//         console.log("en fazla 50 sayısına kadar hesaplanır" + user);
+//         break;
+//     }
+//     if (i===7) {
+//         console.log("7 sayısını toplama!!!" +user);
+// break;        
+//     }
+//     }
+// }
+// sumExamplesData();
